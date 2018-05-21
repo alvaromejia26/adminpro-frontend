6daeff8e48b14aeaccf5714054468e3839c9c1cf
Alvaro Mejia <alvaromejia26@gmail.com>
2018-05-20 21:05:23 -0500

    Breadcrumbs, metaTags, título de página, promesas y observables

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -17,6 +17,8 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
 
 // ng2-charts
 import { ChartsModule } from 'ng2-charts';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 @NgModule({
     declarations: [
@@ -26,7 +28,9 @@ import { ChartsModule } from 'ng2-charts';
         Graficas1Component,
         IncrementadorComponent,
         GraficoDonaComponent,
-        AccountSettingsComponent
+        AccountSettingsComponent,
+        PromesasComponent,
+        RxjsComponent
     ],
     exports: [
         DashboardComponent,
